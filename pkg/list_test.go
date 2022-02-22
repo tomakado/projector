f12de11d85cf8b3d9a962428ab7b6b96b5ed5ca9
Ildar Karymov <ildarkarymoff@gmail.com>
2022-02-23 02:47:28 +0300

    Update list test

--- pkg/list_test.go
+++ pkg/list_test.go
@@ -8,9 +8,17 @@ import (
 )
 
 func TestCollectEmbeddedManifests(t *testing.T) {
-	expected := []string{"go/hello-world", "projector-template"}
+	t.Run("CollectEmbeddedManifests returns a slice of manifests on valid passed root", func(t *testing.T) {
+		expected := []string{"go/hello-world", "projector-template"}
 
-	manifests, err := projector.CollectEmbeddedManifests(&embeddedTestData, "testdata/embed/", ".")
-	require.NoError(t, err)
-	require.Equal(t, expected, manifests)
+		manifests, err := projector.CollectEmbeddedManifests(&embeddedTestData, "testdata/embed/", ".")
+		require.NoError(t, err)
+		require.Equal(t, expected, manifests)
+	})
+
+	t.Run("CollectEmbeddedManifests returns error in invalid passed root", func(t *testing.T) {
+		manifests, err := projector.CollectEmbeddedManifests(&embeddedTestData, "C:/Users/testdata/hello", "/var/lib")
+		require.Error(t, err)
+		require.Nil(t, manifests)
+	})
 }
