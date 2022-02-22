af051834d03e7459cad7917c780a48a76fb5f7e7
Ildar Karymov <ildarkarymoff@gmail.com>
2022-02-23 02:40:53 +0300

    Remove unused function `collectManifests`

--- cmd/list.go
+++ cmd/list.go
@@ -2,8 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"path"
-	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/tomakado/projector/internal/pkg/verbose"
@@ -29,37 +27,4 @@ func runList(_ *cobra.Command, _ []string) error {
 	}
 
 	return nil
-}
-
-func collectManifests(root string) ([]string, error) {
-	verbose.Printf("reading %q", root)
-
-	var manifests []string
-
-	dirs, err := resources.ReadDir(root)
-	if err != nil {
-		return nil, fmt.Errorf("read dir %q: %w", root, err)
-	}
-
-	for _, entry := range dirs {
-		if !entry.IsDir() {
-			if entry.Name() == "projector.toml" {
-				manifestName := strings.TrimPrefix(root, embedRoot)
-				manifests = append(manifests, manifestName) //nolint:staticcheck
-
-				verbose.Printf("projector.toml detected, so registered %q as manifest", manifestName)
-			}
-
-			continue
-		}
-
-		children, err := collectManifests(path.Join(root, entry.Name()))
-		if err != nil {
-			return nil, err
-		}
-
-		manifests = append(manifests, children...)
-	}
-
-	return manifests, nil
 }
